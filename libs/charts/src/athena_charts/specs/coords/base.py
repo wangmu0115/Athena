c8045944579c500c2dfeedad6b92ff023dfc9a0e
wangmu <wangmu8889@gmail.com>
2026-05-28 00:16:42 +0800

    remove athena-charts

--- libs/charts/src/athena_charts/specs/coords/base.py
+++
@@ -1,8 +0,0 @@
-from pydantic import Field
-
-from athena_charts.specs._base import _BaseSpec
-from athena_charts.specs.coords.types import CoordKind
-
-
-class Coord(_BaseSpec):
-    kind: CoordKind = Field(..., description="坐标系统类型")
