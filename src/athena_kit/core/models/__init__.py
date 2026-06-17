fd88da02f5131a4230f6c53b2e4b5e0e0b60e10e
wangmu0115 <wangmu8889@gmail.com>
2026-06-17 16:54:05 +0800

    add export

--- src/athena_kit/core/models/__init__.py
+++ src/athena_kit/core/models/__init__.py
@@ -5,17 +5,22 @@ from athena_kit._import_utils import import_attr
 if TYPE_CHECKING:
     from athena_kit.core.models.base import BaseAthenaModel
     from athena_kit.core.models.enums import LabelIntEnum, LabelStrEnum
+    from athena_kit.core.models.mapping import model_to_model, models_to_model
 
 __all__ = (
     "BaseAthenaModel",
     "LabelIntEnum",
     "LabelStrEnum",
+    "model_to_model",
+    "models_to_model",
 )
 
 _dynamic_imports = {
     "BaseAthenaModel": "base",
     "LabelIntEnum": "enums",
     "LabelStrEnum": "enums",
+    "model_to_model": "mapping",
+    "models_to_model": "mapping",
 }
 
 
